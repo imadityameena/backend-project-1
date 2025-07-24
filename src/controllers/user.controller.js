c5433429dc9f46c8d16e10789f92823a097b9f2c
Aditya Meena <adityameena2233@gmail.com>
2025-07-25 00:36:25 +0530

    added re-getting the access token using refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import {ApiResponse} from "../utils/ApiResponse.js"
-
+import jwt from 'jsonwebtoken'
 
 const generateAccessAndRefreshTokens = async(userId) => {
   try {
@@ -113,17 +113,6 @@ return res.status(201).json(
   new ApiResponse(201, createdUser, "User registered Successfully")
 )
 
-
-
-
-
-
-
-  
-
-
-
-
 })
 
 const loginUser = asyncHandler(async (req, res) =>
@@ -138,8 +127,8 @@ const loginUser = asyncHandler(async (req, res) =>
 
 
   const {email, username, password} = req.body
-
-  if(!username || !email){
+  //if (!username && !email)
+  if(!(username || email)){
     throw new ApiError(400,  "username or email is required")
   }
 
@@ -209,8 +198,63 @@ const logoutUser = asyncHandler(async(req,res) => {
   .json(new ApiResponse(200, {}, "User logged out"))
 })
 
+
+const refreshAccessToken = asyncHandler(async(req, res) => {
+  const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken
+
+  if(!incomingRefreshToken){
+    throw new ApiError(401, "unauthorized request")
+
+  }
+
+
+  try {
+
+    const decodedToken =  jwt.verify(
+      incomingRefreshToken, 
+      process.env.REFRESH_TOKEN_SECRET
+    )
+
+    const user = await User.findById(decodedToken?._id) 
+
+    if(!user){
+      throw new ApiError(401, "Invalid refresh token")
+    }
+
+    if(incomingRefreshToken !== user?.refreshToken){
+      throw new ApiError(401, "Refresh token is expired or used")
+    }
+
+
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id) //this is a example of object destructuring in JS
+    //now the value of accessToken and refreshToken in generateAccessAndRefreshTokens will get updated as our new provided values we have provided in the const { '_', '_' }
+
+    return res
+    .status(200)
+    .cookie('accessToken', accessToken, options)
+    .cookie('refreshToken', refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {accessToken, refreshToken: refreshToken},
+        "Access token refreshed"
+      )
+    )
+  } catch (error) {
+
+    throw new ApiError(401, error?.message || "Invalid refresh token")
+    
+  }
+})
+
 export {
   registerUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  refreshAccessToken
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 
 
@@ -21,6 +21,13 @@ upload.fields([
 registerUser
 
 )
+
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT, logout)
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 
